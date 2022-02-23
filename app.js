4ca7d5b3c14a42f9f76ceabaa4708400f77179e1
peeyush-k-madhavan <madhavanpeeyush@gmail.com>
2022-02-23 13:00:42 +0530

    Added Homepage, Login and Register

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+const expressHandlebars = require("express-handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "home" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("home/index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("home/about");
+});
+
+app.get("/login", (req, res) => {
+  res.render("home/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("home/register");
+});
+
 app.listen(4500, () => {
   console.log(`Listening on Port 4500`);
 });
