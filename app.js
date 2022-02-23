793313bdd372bce84c9cdbfe92271ef6e49ee96d
peeyush-k-madhavan <madhavanpeeyush@gmail.com>
2022-02-23 14:49:00 +0530

    Admin Setup

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const expressHandlebars = require("express-handlebars");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+//Set View Engine
 app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "home" }));
 app.set("view engine", "handlebars");
 
-const main = require("./routes/home/main");
-app.use("/", main);
+//Load Routes
+const home = require("./routes/home/index");
+const admin = require("./routes/admin/index");
+
+//Use Routes
+app.use("/", home);
+app.use("/admin", admin);
 
 app.listen(4500, () => {
   console.log(`Listening on Port 4500`);
