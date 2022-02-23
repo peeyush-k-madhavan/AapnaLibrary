e6af31311cec5075630bdc49edc58a832c86e1c4
peeyush-k-madhavan <madhavanpeeyush@gmail.com>
2022-02-23 13:26:05 +0530

    Added Home Routes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
 const expressHandlebars = require("express-handlebars");
 
@@ -9,21 +8,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "home" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-  res.render("home/index");
-});
-
-app.get("/about", (req, res) => {
-  res.render("home/about");
-});
-
-app.get("/login", (req, res) => {
-  res.render("home/login");
-});
-
-app.get("/register", (req, res) => {
-  res.render("home/register");
-});
+const main = require("./routes/home/main");
+app.use("/", main);
 
 app.listen(4500, () => {
   console.log(`Listening on Port 4500`);
