c92f43344cc874c770de0db59bd54c415fc30625
peeyush-k-madhavan <madhavanpeeyush@gmail.com>
2022-02-25 09:33:02 +0530

    MongoDB Connected

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const expressHandlebars = require("express-handlebars");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/AAPNALibrary")
+  .then((db) => {
+    console.log("MongoDB Connected");
+  })
+  .catch((error) => console.log(error));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,10 +20,12 @@ app.set("view engine", "handlebars");
 //Load Routes
 const home = require("./routes/home/index");
 const admin = require("./routes/admin/index");
+const posts = require("./routes/admin/posts");
 
 //Use Routes
 app.use("/", home);
 app.use("/admin", admin);
+app.use("/admin/posts", posts);
 
 app.listen(4500, () => {
   console.log(`Listening on Port 4500`);
