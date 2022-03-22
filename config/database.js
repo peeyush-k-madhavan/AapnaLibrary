15273f446e00c88bf85b39c8f4ad4e1771a0a44a
peeyush-k-madhavan <madhavanpeeyush@gmail.com>
2022-03-22 10:01:40 +0530

    DB Connected to Cloud

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoDBUrl: "mongodb://127.0.0.1:27017/AAPNALibrary",
+  // mongoDBUrl: "mongodb://127.0.0.1:27017/AAPNALibrary",
+  mongoDBUrl:
+    "mongodb+srv://peeyush:iJXj6ADouvY5kJGn@cluster0.iscie.mongodb.net/AAPNALibrary?retryWrites=true&w=majority",
 };
