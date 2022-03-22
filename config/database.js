165af076fe2e9fad181ec85b3d85637798ab35db
peeyush-k-madhavan <madhavanpeeyush@gmail.com>
2022-03-22 10:24:35 +0530

    Fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
-  // mongoDBUrl: "mongodb://127.0.0.1:27017/AAPNALibrary",
-  mongoDBUrl:
-    "mongodb+srv://peeyush:iJXj6ADouvY5kJGn@cluster0.iscie.mongodb.net/AAPNALibrary?retryWrites=true&w=majority",
+  mongoDBUrl: "mongodb://127.0.0.1:27017/AAPNALibrary",
 };
